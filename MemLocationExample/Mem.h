7b6284f928cfc89f75638107fe4ad6f257780390
Luis Fraguada <luis@mcneel.com>
2018-01-31 18:00:18 +0100

    First Commit

---
+++ MemLocationExample/Mem.h
@@ -0,0 +1,6 @@
+#ifndef __MEMORY_LOCATIONS__
+#define __MEMORY_LOCATIONS__
+
+#define NODE_ID 0  // one byte with a magic value
+
+#endif
---
+++ MemLocationExample/MemLocationExample.ino
@@ -0,0 +1,14 @@
+#include "Mem.h"
+
+void setup(){
+  Serial.begin(9600);
+  
+  Serial.println(F("[Llum Installation Ball Node v0.4]"));
+  Serial.print(F("Node Id: "));
+  Serial.println(NODE_ID);
+  
+}
+
+void loop(){
+}
+
